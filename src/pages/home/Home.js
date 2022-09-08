cf6a717a4958e56bf77c25309d5927354a5e042b
William-silva-Developer <98723501+William-silva-Developer@users.noreply.github.com>
2022-09-08 10:09:41 -0300

    Update Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -14,32 +14,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Pages() {
 
-  const [filmes, setFilmes] = useState([ {
-    "rank": "1",
-    "title": "The Shawshank Redemption",
-    "fullTitle": "The Shawshank Redemption (1994)",
-    "image": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
-    "crew": "Frank Darabont (dir.), Tim Robbins, Morgan Freeman",
-    "id": "1"
-   },
-   {
-    "rank": "2",
-    "title": "The Godfather",
-    "fullTitle": "The Godfather (1972)",
-    "image": "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX128_CR0,1,128,176_AL_.jpg",
-    "crew": "Francis Ford Coppola (dir.), Marlon Brando, Al Pacino",
-    "id": "2"
-   },
-   {
-    "rank": "3",
-    "title": "The Dark Knight",
-    "fullTitle": "The Dark Knight (2008)",
-    "image": "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX128_CR0,3,128,176_AL_.jpg",
-    "crew": "Christopher Nolan (dir.), Christian Bale, Heath Ledger",
-    "id": "3"
-   },
-
-  ]);
+  const [filmes, setFilmes] = useState([ ]);
 
   useEffect(()=>{
 
@@ -56,7 +31,7 @@ export default function Pages() {
         
       }
     }
-   //getFilmes();
+   getFilmes();
 
   },[]);
 
